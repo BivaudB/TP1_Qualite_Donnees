2fe337e01d70c6d23c6169cc59103906b667cba8
Baptiste <baptiste.bivaud@hotmail.fr>
2021-01-27 16:31:43 +0100

    Traitement des erreurs

--- tp_erreur.py
+++ tp_erreur.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import mplcursors
 
 data_climat = pd.read_excel("./graph/Climat.xlsx",sheet_name='SI -erreur')
 df = pd.DataFrame(data_climat)
 
 # Correction des valeurs en erreur
-# for month in range(len(df.count())):
-#     for day in range(len(df.iloc[:, month])):
-#         if df.iloc[day, month] == "0xFFFF" or df.iloc[day, month] == "Sun":
-#             df.iloc[day, month] = (df.iloc[day+1, month] + df.iloc[day-1, month])/2
+for month in range(len(df.count())):
+    for day in range(len(df.iloc[:, month])):
+        if df.iloc[day, month] == "0xFFFF" or df.iloc[day, month] == "Sun":
+            df.iloc[day, month] = (df.iloc[day+1, month] + df.iloc[day-1, month])/2
 
 fig, (ax1, ax2) = plt.subplots(2)
 names = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
@@ -30,10 +31,19 @@ min_max = df.agg(["min","max"])
 
 #Courbe pour chaque mois
 month_filter = 0
-for month in range(len(df.count())):
+liste = []
+for month in range(len(df.count())-1):
+    plt.suptitle(names[month_filter])
     month_filter = month_filter+1
-    plt.suptitle(names[month])
-    plt.xlabel('Jours de ' + names[month])
+    plt.xlabel('Jours')
     plt.ylabel('Température (°C)')
     plt.plot(df.iloc[:, month_filter])
+    mplcursors.cursor()
     plt.show()
+    for day in range(len(df.iloc[:, month_filter])):
+        plt.xlabel('Jours de l\'année')
+        plt.ylabel('Température (°C)')
+        liste.append(df.iloc[day, month_filter])
+plt.plot(liste)
+mplcursors.cursor()
+plt.show()
